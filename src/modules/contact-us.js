6b4cdb258fd1ba35c45e4e9f6b80f206dcaddd79
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-22 04:06:12 +0600

    Add contact-us.js

--- src/modules/contact-us.js
+++ src/modules/contact-us.js
@@ -3,6 +3,27 @@ function createContactCopy() {
     copyTextDiv.setAttribute('id', 'contact-copy-div');
     copyTextDiv.setAttribute('class', 'fade-in-right');
 
+    const contactUsHeading = document.createElement('h1');
+    contactUsHeading.textContent = "Find Us Here"
+    contactUsHeading.setAttribute('id', 'contact-us-heading')
+    const days = document.createElement('p');
+    const hours = document.createElement('p');
+    const address = document.createElement('p');
+    days.setAttribute('id', 'days')
+    hours.setAttribute('id', 'hours');
+    address.setAttribute('id', 'address');
+    days.textContent = "Saturday - Friday";
+    hours.textContent = "Lunch: 1pm - 4pm  |  Dinner: 7pm - 10pm"
+    address.textContent = "34/3, Satmasjid Road, Dhanmondi, Dhaka, Bangladesh."
+
+    copyTextDiv.appendChild(contactUsHeading);
+    copyTextDiv.appendChild(days);
+    copyTextDiv.appendChild(hours);
+    copyTextDiv.appendChild(address);
+
+
+    return copyTextDiv;
+
     
 
 
