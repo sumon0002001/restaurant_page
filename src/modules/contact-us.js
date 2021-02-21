4af2b7c1cc76cd9ee0bec5cb19813c2810b906ec
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-22 03:12:39 +0600

    Add contact.js file to module

---
+++ src/modules/contact-us.js
@@ -0,0 +1,9 @@
+function createContactCopy() {
+    const copyTextDiv = document.createElement('div');
+    copyTextDiv.setAttribute('id', 'contact-copy-div');
+    copyTextDiv.setAttribute('class', 'fade-in-right');
+
+    
+
+
+}
