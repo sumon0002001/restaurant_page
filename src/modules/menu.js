c7da296d6644f35188692626e35d74cb800db3c2
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-22 18:54:23 +0600

    Fix spacing in menu.js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,31 +1,26 @@
 function createMenu(className) {
   const parentDiv = document.createElement('div');
   parentDiv.className = 'menu-parent';
-
   const menu = document.createElement('img');
   menu.setAttribute('class', className);
-  menu.src = '../dist/images/Menu-Desktop.svg';
-
+  menu.src = '../dist/images/res_3.jpg';
   parentDiv.appendChild(menu);
+
   return parentDiv;
 }
 
 function createMobileMenu() {
   const mobileMenuDiv = document.createElement('div');
   mobileMenuDiv.setAttribute('class', 'mobile-menu');
-
   const lunchMenu = document.createElement('img');
-  lunchMenu.src = '../dist/images/res_2';
+  lunchMenu.src = '../dist/images/res_2.jpg';
   lunchMenu.setAttribute('id', 'lunch');
-
   const dinnerMenu = document.createElement('img');
-  dinnerMenu.src = '../dist/images/res_3';
+  dinnerMenu.src = '../dist/images/res_3.jpg';
   dinnerMenu.setAttribute('id', 'dinner');
-
   const drinksMenu = document.createElement('img');
-  drinksMenu.src = '../dist/images/res_4';
+  drinksMenu.src = '../dist/images/res_4.jpg';
   drinksMenu.setAttribute('id', 'drinks');
-
   mobileMenuDiv.appendChild(lunchMenu);
   mobileMenuDiv.appendChild(dinnerMenu);
   mobileMenuDiv.appendChild(drinksMenu);
@@ -40,14 +35,11 @@ function loadMenu() {
   contactMenu.innerHTML = '';
   homeMenu.style.display = 'none';
   contactMenu.style.display = 'none';
-
   const menuTab = document.getElementById('menuMenu'); 
   menuTab.innerHTML = '';
   menuTab.style.display = 'flex';
-
   const menuGrid = createMenu('menu-img');
   menuTab.appendChild(menuGrid);
-
   const mobileMenus = createMobileMenu();
   menuTab.appendChild(mobileMenus);
 }
