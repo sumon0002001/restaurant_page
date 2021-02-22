8bf68666479a24d2fb50d69eefe3a0c859f0b8d9
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-22 12:16:59 +0600

    Add contact-us.js

--- src/modules/contact-us.js
+++ src/modules/contact-us.js
@@ -21,10 +21,36 @@ function createContactCopy() {
     copyTextDiv.appendChild(hours);
     copyTextDiv.appendChild(address);
 
-
-    return copyTextDiv;
-
-    
-
-
+  return copyTextDiv;
 }
+
+function addImage() {
+  const contatctUsImg = document.createElement('img');
+  contatctUsImg.setAttribute('id', 'contact-img');
+  contatctUsImg.src = '../dist/images/res_4.jpg';
+
+  return contatctUsImg;
+}
+
+function loadContactPage() {
+    const homeMenu = document.getElementById('homeMenu');
+    const menuMenu = document.getElementById('menuMenu');
+    homeMenu.innerHTML = '';
+    menuMenu.innerHTML = '';
+    homeMenu.style.display = 'none';
+    menuMenu.style.display = 'none';
+    const contactUsTab = document.getElementById('contactUsMenu');
+    contactUsTab.innerHTML = '';
+    contactUsTab.style.display = 'flex';
+    const textBox = createContactCopy();
+    contactUsTab.appendChild(textBox);
+    const rooftopBarImg = addImage();
+    rooftopBarImg.setAttribute('id', 'bar-img');
+    contatctUsImg.appendChild(rooftopBarImg);
+    const mobileImg = document.createElement('img');
+    mobileImg.setAttribute('id', 'mobile-img');
+    mobileImg.src = '../dist/imgs/rooftop-bar.jpg';
+    contactUsTab.appendChild(mobileImg)
+}
+
+export default loadContactPage;
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,58 +1,55 @@
 function createMenu(className) {
-    const parentDiv = document.createElement('div');
-    parentDiv.className = 'menu-parent';
+  const parentDiv = document.createElement('div');
+  parentDiv.className = 'menu-parent';
 
-    const menu = document.createElement('img');
-    menu.setAttribute('class', className);
-    menu.src = '../dist/images/Menu-Desktop.svg';
+  const menu = document.createElement('img');
+  menu.setAttribute('class', className);
+  menu.src = '../dist/images/Menu-Desktop.svg';
 
-    parentDiv.appendChild(menu);
-    return parentDiv;
+  parentDiv.appendChild(menu);
+  return parentDiv;
 }
 
 function createMobileMenu() {
-    const mobileMenuDiv = document.createElement('div');
-    mobileMenuDiv.setAttribute('class', 'mobile-menu');
+  const mobileMenuDiv = document.createElement('div');
+  mobileMenuDiv.setAttribute('class', 'mobile-menu');
 
-    const lunchMenu = document.createElement('img');
-    lunchMenu.src = '../dist/images/res_2';
-    lunchMenu.setAttribute('id', 'lunch');
+  const lunchMenu = document.createElement('img');
+  lunchMenu.src = '../dist/images/res_2';
+  lunchMenu.setAttribute('id', 'lunch');
 
-    const dinnerMenu = document.createElement('img');
-    dinnerMenu.src = '../dist/images/res_3';
-    dinnerMenu.setAttribute('id', 'dinner');
+  const dinnerMenu = document.createElement('img');
+  dinnerMenu.src = '../dist/images/res_3';
+  dinnerMenu.setAttribute('id', 'dinner');
 
-    const drinksMenu = document.createElement('img');
-    drinksMenu.src = '../dist/images/res_4';
-    drinksMenu.setAttribute('id', 'drinks');
+  const drinksMenu = document.createElement('img');
+  drinksMenu.src = '../dist/images/res_4';
+  drinksMenu.setAttribute('id', 'drinks');
 
-    mobileMenuDiv.appendChild(lunchMenu);
-    mobileMenuDiv.appendChild(dinnerMenu);
-    mobileMenuDiv.appendChild(drinksMenu);
+  mobileMenuDiv.appendChild(lunchMenu);
+  mobileMenuDiv.appendChild(dinnerMenu);
+  mobileMenuDiv.appendChild(drinksMenu);
 
-    return mobileMenuDiv;
+  return mobileMenuDiv;
 }
 
 function loadMenu() {
-    // Close and clear other menus
-    const homeMenu = document.getElementById('homeMenu');
-    const contactMenu = document.getElementById('contactUsMenu');
-    homeMenu.innerHTML = '';
-    contactMenu.innerHTML = '';
-    homeMenu.style.display = 'none';
-    contactMenu.style.display = 'none';
-    // open and clear this tab
-    const menuTab = document.getElementById('menuMenu'); 
-    menuTab.innerHTML = '';
-    menuTab.style.display = 'flex';
+  const homeMenu = document.getElementById('homeMenu');
+  const contactMenu = document.getElementById('contactUsMenu');
+  homeMenu.innerHTML = '';
+  contactMenu.innerHTML = '';
+  homeMenu.style.display = 'none';
+  contactMenu.style.display = 'none';
 
-    //normal menu
-    const menuGrid = createMenu('menu-img');
-    menuTab.appendChild(menuGrid);
+  const menuTab = document.getElementById('menuMenu'); 
+  menuTab.innerHTML = '';
+  menuTab.style.display = 'flex';
 
-    //mobile menus
-    const mobileMenus = createMobileMenu();
-    menuTab.appendChild(mobileMenus);
+  const menuGrid = createMenu('menu-img');
+  menuTab.appendChild(menuGrid);
+
+  const mobileMenus = createMobileMenu();
+  menuTab.appendChild(mobileMenus);
 }
 
 export default loadMenu;
