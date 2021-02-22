aca47635b7b80a431ba8014a8b4e5b780578e955
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-02-22 18:40:58 +0600

    Fix space in home.js

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,7 @@
 function createTextDiv(id) {
-    const homeMenuText = document.createElement('div');
-    homeMenuText.setAttribute('id', id);
-    return homeMenuText;
+  const homeMenuText = document.createElement('div');
+  homeMenuText.setAttribute('id', id);
+  return homeMenuText;
 }
 
 function createHomePageHeading(id) {
@@ -10,7 +10,6 @@ function createHomePageHeading(id) {
   const niceUnderline = document.createElement('span');
   niceUnderline.className = 'underline--magical';
   niceUnderline.textContent = 'The Best Bangladeshi Restaurant';
-
   headingDiv.appendChild(niceUnderline);    
 
   return headingDiv;
@@ -19,63 +18,53 @@ function createHomePageHeading(id) {
 function createCopyContent(id) {
   const copy = document.createElement('p');
   copy.setAttribute('id', id);
-  copy.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum efficitur maximus. " + 
-                     "Nulla mollis leo dui, et volutpat nulla ultrices ac. Ut venenatis quis felis aliquam porta. " + 
-                     "Morbi eu vulputate nulla. Mauris pulvinar, urna id rhoncus tempor, enim mauris porta mi, eget " 
-                     "dignissim neque magna in arcu. Curabitur convallis nunc ac sollicitudin consectetur.";
+  copy.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum efficitur maximus. ' + 
+                     'Nulla mollis leo dui, et volutpat nulla ultrices ac. Ut venenatis quis felis aliquam porta. ' + 
+                     'Morbi eu vulputate nulla. Mauris pulvinar, urna id rhoncus tempor, enim mauris porta mi, eget ' 
+                     'dignissim neque magna in arcu. Curabitur convallis nunc ac sollicitudin consectetur.';
   return copy;
 }
 
 function addImagesToHome(className) {
-    const imgDiv = document.createElement('div');
-    imgDiv.setAttribute('class', 'images');
-
-    const imgOne = document.createElement('img');
-
-    imgOne.setAttribute('class', className);
-    imgOne.setAttribute('id', 'images');
-
-    imgOne.src = '../dist/images/res_11.jpg';
-
-    imgDiv.appendChild(imgOne);
+  const imgDiv = document.createElement('div');
+  imgDiv.setAttribute('class', 'images');
+  const imgOne = document.createElement('img');
+  imgOne.setAttribute('class', className);
+  imgOne.setAttribute('id', 'images');
+  imgOne.src = '../dist/images/res_11.jpg';
+  imgDiv.appendChild(imgOne);
    
-    return imgDiv;
+  return imgDiv;
 }
 
 function addReservationButton(className) {
-    const reservationButton = document.createElement('button');
- 
-    reservationButton.innerHTML = "Make a Reservation";
-    reservationButton.id = "reservation-button";
-    reservationButton.className = 'reservation-button';
-    return reservationButton;
+  const reservationButton = document.createElement('button');
+  reservationButton.innerHTML = 'Make a Reservation';
+  reservationButton.className = 'reservation-button';
+
+  return reservationButton;
 }
 
 function loadHome() {
-    // Close and clear other menus
-    const menuMenu = document.getElementById('menuMenu');
-    const contactMenu = document.getElementById('contactUsMenu');
-    menuMenu.innerHTML = "";
-    contactMenu.innerHTML = "";
-    menuMenu.style.display = 'none';
-    contactMenu.style.display = 'none';
-    
-    // open and clear this tab
-    const openTab = document.getElementById('homeMenu');
-    openTab.innerHTML = "";
-    openTab.style.display = 'flex';
+  const menuMenu = document.getElementById('menuMenu');
+  const contactMenu = document.getElementById('contactUsMenu');
+  menuMenu.innerHTML = '';
+  contactMenu.innerHTML = '';
+  menuMenu.style.display = 'none';
+  contactMenu.style.display = 'none';
+  const openTab = document.getElementById('homeMenu');
+  openTab.innerHTML = '';
+  openTab.style.display = 'flex';
 
-    const textDiv = createTextDiv('home-text-box');  
-    const copy = createCopyContent('home-copy');
-    const heading = createHomePageHeading('home-heading');
-    const imgs = addImagesToHome('home-img');
-    const button = addReservationButton('reservation');
+  const textDiv = createTextDiv('home-text-box');  
+  const copy = createCopyContent('home-copy');
+  const imgs = addImagesToHome('home-img');
+  const button = addReservationButton('reservation');
 
-    textDiv.appendChild(heading);
-    textDiv.appendChild(copy);
-    textDiv.appendChild(button);
-    openTab.appendChild(imgs);
-    openTab.appendChild(textDiv);
+  textDiv.appendChild(copy);
+  textDiv.appendChild(button);
+  openTab.appendChild(imgs);
+  openTab.appendChild(textDiv);
 }
 
 export default loadHome;
